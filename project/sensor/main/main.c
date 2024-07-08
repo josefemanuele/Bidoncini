c47e863fd41fb80f0cb82d99fa7900e438d51823
Simona De Risi <derisi.2081525@studenti.uniroma1.it>
2024-07-08 12:32:41 +0200

    update README

--- project/sensor/main/main.c
+++ project/sensor/main/main.c
@@ -14,7 +14,7 @@ char *TAG_MAIN_SENSOR = "MAIN_SENSOR";
 int FREQUENCY_ANALYSIS_SLEEP_S = 10;    //time between measurements in the frequency analysis
 RTC_DATA_ATTR int WAKEUP_DELAY_S;       //time between measurements
 RTC_DATA_ATTR int MEASUREMENT_NUMBER = 0;       //number of measurement since last time has been emptied
-RTC_DATA_ATTR int EXPECTED_MEASUREMENTS;        //expected number of measurement to do until trash bin is full
+int EXPECTED_MEASUREMENTS = 4;        //expected number of measurement to do until trash bin is full
 RTC_DATA_ATTR bool need_frequency_analysis = true;
 int HEIGHT_BIN_CM = 40;
 
@@ -55,14 +55,13 @@ void deep_sleep(void)
 */
 void frequency_analysis(){
     while (1){
-        int distance = 1;
+        int distance = measure();
         MEASUREMENT_NUMBER++;
+        lora_message_send(ID, distance);
         //check if the bin is at least 90% full
         if ((distance/HEIGHT_BIN_CM)*100 <= 10){
-            WAKEUP_DELAY_S = (MEASUREMENT_NUMBER * WAKEUP_DELAY_S) / 4;     //update the time interval between measurements
-            EXPECTED_MEASUREMENTS = MEASUREMENT_NUMBER;     //update the expected number of measurements until the bin is full
+            WAKEUP_DELAY_S = (MEASUREMENT_NUMBER * WAKEUP_DELAY_S) / EXPECTED_MEASUREMENTS;     //update the time interval between measurements
             need_frequency_analysis = false;
-            MEASUREMENT_NUMBER = 0;
             break;
         }
         else {
