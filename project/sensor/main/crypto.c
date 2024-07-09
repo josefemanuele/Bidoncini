7f4088dcd4a51d615c3c7878e3648f9b3f283b2a
Simona De Risi <derisi.2081525@studenti.uniroma1.it>
2024-07-10 00:50:30 +0200

    crypto fixed

--- project/sensor/main/crypto.c
+++ project/sensor/main/crypto.c
@@ -1,8 +1,3 @@
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "esp_log.h"
 /* WOLFSSL includes. */
 #define WOLFSSL_ESPIDF
 #define WOLFSSL_USER_SETTINGS
@@ -10,16 +5,27 @@
 #include <wolfssl/wolfcrypt/random.h>
 #include <wolfssl/wolfcrypt/curve25519.h>
 #include <wolfssl/wolfcrypt/ecc.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "esp_log.h"
+
 #include "headers/crypto.h"
 
-int LEN = 128;      //length of ECC key
-RTC_DATA_ATTR byte my_key_public[LEN];
-RTC_DATA_ATTR byte my_key_private[LEN];
+#define LEN 128      //length of ECC key
+#define BYTES_PER_LINE 6
+byte my_key_public[LEN];
+byte my_key_private[LEN];
+RTC_DATA_ATTR bool key_generated = false;
 RTC_DATA_ATTR ecc_key my_key;
-RTC_DATA_ATTR ecc_key server_pub[LEN];
+RTC_DATA_ATTR ecc_key server_pub;
 
-byte server_public_key_bytes[LEN] = "0xababab";
+byte server_public_key_bytes[LEN] = "0x0100000001000000000000000000000060E8033C00000000000000000000000000000000A5A5A5A5A5A5A5A5A5A5A5A5A5A5A5A50
+                                    10000006039CF3F430000006039CF3F5C303880903ACF3F285F02428C3F033CF03ACF3FD03ACF3F0C000000A5A5A5A5D03ACF3F0C000000
+                                    1F0000000000000021660082C03ACF3F03000000";
 char *TAG_CRYPTO = "CRYPTO_MODULE";
+
 
 void print_bytes(const uint8_t *bytes, int len)
 {
@@ -36,8 +42,14 @@ void print_bytes(const uint8_t *bytes, int len)
 }
 
 void generate_keys(){
+    //check if my key has already been produced
+    if(key_generated){
+        return;
+    }
+
     ecc_key key;
     WC_RNG rng;
+    word32 len = LEN;
 
     //initialize ECC key and RNG
     ESP_LOGI(TAG_CRYPTO, "Initializing ECC key and RNG");
@@ -47,10 +59,11 @@ void generate_keys(){
     ESP_LOGI(TAG_CRYPTO, "Generating ECC key");
     wc_ecc_make_key(&rng, 32, &key);
     my_key = key;
+    key_generated = true;
     //export ECC key
     ESP_LOGI(TAG_CRYPTO, "Exporting public and private key");
-    wc_ecc_export_x963(&key, my_key_public, &LEN);
-    wc_ecc_export_private_only(&key, my_key_private, &LEN);
+    wc_ecc_export_x963(&key, my_key_public, &len);
+    wc_ecc_export_private_only(&key, my_key_private, &len);
 
     ESP_LOGI(TAG_CRYPTO, "Printing public key");    
     print_bytes(my_key_public, LEN);
@@ -63,8 +76,8 @@ void generate_keys(){
     //generatig ECC key
     wc_ecc_make_key(&rng, 32, &key2);
     //export ECC key
-    wc_ecc_export_x963(&key2, server_key_public, &LEN);
-    wc_ecc_export_private_only(&key2, server_key_private, &LEN);
+    wc_ecc_export_x963(&key2, server_key_public, &len);
+    wc_ecc_export_private_only(&key2, server_key_private, &len);
 
     print_bytes(server_key_public, LEN);
     printf("\n");
@@ -79,11 +92,16 @@ void generate_keys(){
     wc_ecc_import_x963(server_public_key_bytes, sizeof(server_public_key_bytes), &server_pub);
 }
 
-void encrypt_value(char* message, unsigned char* encrypted, word32 encryptedSz){
+void encrypt_value(char* message, word32 msgLen, unsigned char* encrypted, word32 encryptedSz){
     const unsigned char* msg; 
-    word32 msgLen;
+    WC_RNG rng;
+    ecEncCtx* ctx;      //ECC context object
+    ESP_LOGI(TAG_CRYPTO, "Initializinfg ecEncCtx");
+    wc_InitRng(&rng);
+    ctx = wc_ecc_ctx_new(REQ_RESP_CLIENT, &rng);
+    wc_ecc_ctx_set_algo(ctx, ecAES_128_CTR, ecHKDF_SHA256, ecHMAC_SHA256);
+    ESP_LOGI(TAG_CRYPTO, "Initialized ecEncCtx");
     msg = (unsigned char*)message;
-    msgLen = (word32)strlen((const char*)msg);
-    encryptedSz = sizeof(encrypted);
-    //wc_ecc_encrypt(&my_key, &server_pub, msg, msgLen, encrypted, &encryptedSz);
+    ESP_LOGI(TAG_CRYPTO, "before wc_ecc_encrypt %d", msgLen);
+    wc_ecc_encrypt(&my_key, &server_pub, msg, msgLen, encrypted, &encryptedSz, ctx);
 }
